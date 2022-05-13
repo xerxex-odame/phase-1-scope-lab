5407b46678efc160c3760e49d98c1567cbfb2f2c
xerxex-odame <kingodame@yahoo.com>
2022-05-13 09:18:34 +0000

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'lordia';
+const leastFavoriteCustomer = 'lordina';
+
+
+function upperCaseCustomerName(){
+
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not lordia';
+  
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe lordia';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'cindy';
+}
+
+console.log(changeLeastFavoriteCustomer());
